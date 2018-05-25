3897b4ad72227781ed8d79c65da92a26599b2dd1
Fangfei Li <36939560+FangfeiLi05@users.noreply.github.com>
2018-05-24 18:20:21 -0700

    Update on 6:00 pm, May 24, 2018

--- xini_opt_m2_complex.m
+++ xini_opt_m2_complex.m
@@ -1,26 +1,30 @@
 function [output] = xini_opt_m2_complex(input)
 % -------------------------------------------------------------------------
 % xini_opt_m2_complex
-% A SUB-FUNCTION USED IN xini_opt_command.m TO CALCULATE LIKELIHOOD VALUE
-% NEEDED IN OPTIMIZATION
+% A SUB-FUNCTION CALLED BY xini_opt_command.m TO CALCULATE THE OPPOSITE VALUE (-y) 
+% OF THE LIKELIHOOD FUNCTION FOR A GENOTYPE GIVEN THE FITNESS OF THE GENOTYPE
+%
 %
 % INPUTS
 % -- input: fitness of a genotype
 %
+%
 % OUTPUTS
-% -- output: likelihood value of a genotype given its fitness
+% -- output: opposite likelihood value (-y) of the genotype
+%
 % -------------------------------------------------------------------------
-
-% [CAN YOU ADD A FEW MORE COMMENTS THROUGHOUT TO EXPLAIN WHAT IS HAPPENING?]
 global t_seq_vec_tempt read_depth_tempt cell_depth_tempt x_mean_est_tempt ...
     r_exp_l kappa_vec_tempt r_est_l_min
-%%
+
 x0_l = input;
 vec_length = length(t_seq_vec_tempt);
-r_est_l = zeros(1, vec_length);
-% r_est_l: estimated read number of a genotype at each sequencing time point
+r_est_l = zeros(1, vec_length); % r_est_l: estimated read number of a genotype 
+                                % at each sequencing time point
 r_est_l(1) = r_exp_l(1);
 
+% Estimate the relative fitness (relative to mean fitness) for a genotype,
+% and calculate the estimated read number of the genotype at all sequencing
+% time points
 for j1 = 2:vec_length
     x_mean_est_interp = interp1([t_seq_vec_tempt(j1-1),t_seq_vec_tempt(j1)],...
         [x_mean_est_tempt(j1-1),x_mean_est_tempt(j1)], ...
@@ -32,6 +36,8 @@ for j1 = 2:vec_length
         (read_depth_tempt(j1-1)*cell_depth_tempt(j1)), r_est_l_min(j1));      
 end
 
+% Calculate the value of the likelihood function for a genotype using the 
+% true and estimated read number data of the genotype
 likelihood_vec_log = nan(1,vec_length);
 pos1 = r_est_l>=20 & r_exp_l>0;
 likelihood_vec_log(pos1) = 1/4*log(r_est_l(pos1))-...
@@ -51,5 +57,4 @@ pos4 = r_est_l>0 & r_est_l<20 & r_exp_l == 0;
 likelihood_vec_log(pos4) = -r_est_l(pos4);
 
 output = -nansum(likelihood_vec_log(2:end));
-    
-
+end
